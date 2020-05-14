d6611e44fe6b2637c9a8fc22a1612f21af0107f6
Nanthawut Norasing <nanthawut.nrs@outlook.com>
2020-05-14 17:37:29 +0700

    Add device plugin

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 /* eslint-disable no-undef */
 import React from 'react';
-// import logo from "./logo.svg";
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header container">
-        <img
-          src="https://icons.iconarchive.com/icons/goescat/macaron/512/stellarium-icon.png"
-          className="App-logo"
-          alt="logo"
-        />
+        <img src={logo} className="App-logo" alt="logo" />
+        <p style={{ wordBreak: 'break-word' }}>{window.cordova && device.platform}</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -19,12 +16,21 @@ function App() {
           Learn React
         </a>
 
-        <p style={marginTop1} id="location"></p>
-        <button className="btn btn-light" style={marginTop1} onClick={() => getCurrentPosition()}>
-          Get Current Position
-        </button>
+        <div className="my-3">
+          <p style={marginTop1} id="location" className="d-none"></p>
+          <div>
+            <div id="location-spinner" className="spinner-border text-light d-none" role="status">
+              <span className="sr-only">Loading...</span>
+            </div>
+          </div>
+          <div>
+            <button className="btn btn-light" style={marginTop1} onClick={() => getCurrentPosition()}>
+              Get Current Position
+            </button>
+          </div>
+        </div>
 
-        <div className={!navigator.camera && 'invisible'}>
+        <div className={'mb-3' + ((!window.cordova || ['Android', 'iPhone'].every(s => s !== device.platform)) && ' d-none')}>
           <div>
             <img
               id="image"
@@ -54,6 +60,17 @@ function App() {
             </div>
           </div>
         </div>
+
+        <div className="row">
+          <div className="col-12" style={{ wordBreak: 'break-word' }}>
+            <p id="qrscanner"></p>
+          </div>
+          <div className="col-12">
+            <button className="btn btn-light" onClick={() => prepareQRScanner()}>
+              QR Code
+            </button>
+          </div>
+        </div>
       </header>
     </div>
   );
@@ -63,11 +80,26 @@ const marginTop1 = { marginTop: '1rem' };
 
 const getCurrentPosition = () => {
   const locationEl = document.getElementById('location');
-  locationEl.innerHTML = 'Getting position...';
+  locationEl.innerHTML = '';
 
-  navigator.geolocation.getCurrentPosition((position) => {
-    locationEl.innerHTML = 'latitude : ' + position.coords.latitude + ', longitude : ' + position.coords.longitude;
-  });
+  const locationSpinnerEl = document.getElementById('location-spinner');
+  locationSpinnerEl.className = locationSpinnerEl.className.replace('d-none', 'd-inline-block').trim();
+
+  navigator.geolocation.getCurrentPosition(
+    (pos) => {
+      locationEl.innerHTML = 'latitude : ' + pos.coords.latitude + ', <br> longitude : ' + pos.coords.longitude;
+      locationEl.className = locationEl.className.replace('d-none', '').trim();
+      locationSpinnerEl.className = locationSpinnerEl.className.replace('d-inline-block', 'd-none').trim();
+    },
+    (err) => {
+      locationEl.innerHTML = err.message;
+      locationEl.className = locationEl.className.replace('d-none', '').trim();
+      locationSpinnerEl.className = locationSpinnerEl.className.replace('d-inline-block', 'd-none').trim();
+    },
+    {
+      timeout: 5000,
+    }
+  );
 };
 
 const getImage = (sourceType) => {
@@ -80,6 +112,27 @@ const getImage = (sourceType) => {
     (err) => {},
     { sourceType: sourceType }
   );
+};
+
+const prepareQRScanner = () => {
+  console.log({ QRScanner });
+  QRScanner.prepare((err, status) => {
+    const qrscannerEl = document.getElementById('qrscanner');
+    if (err) {
+      qrscannerEl.innerHTML = JSON.stringify(err);
+    }
+    qrscannerEl.innerHTML = JSON.stringify({ QRScanner });
+    // if (status.authorized) {
+    //   QRscanner.show();
+    // } else if (status.denied) {
+    //   QRScanner.openSettings();
+    // } else {
+    //   qrscannerEl.innerHTML = JSON.stringify(status);
+    // }
+
+    console.log(`prepareQRScanner -> status`, status);
+    console.log(`prepareQRScanner -> err`, err);
+  });
 };
 
 export default App;
